5fd19eedb990b5b1fec900996f9c722ab12be2ba
Ken Kang <kenjen1617@gmail.com>
2025-08-26 10:55:44 -0400

    API del WEATHER

---
+++ WeatherApiNuevo/Logica.js
@@ -0,0 +1,33 @@
+document.getElementById("obtenerclima").addEventListener("click", function() {
+  const ciudad = document.getElementById("ciudad").value; 
+  const apiKey = "630e169f5b34726d710af64b504f0fae"; 
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric&lang=es`;
+
+ 
+ if (ciudad.trim() === "") {
+    alert("Por favor, rptmai escribe una jodiaa ciudad");
+    return;
+  }
+
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      document.getElementById("resultado").innerHTML = `
+        <h1>${data.name}, ${data.sys.country}</h1>
+        <p> 🌡 Temperatura: ${data.main.temp} °C</p>
+        <p> ☁ Clima: ${data.weather[0].description}</p>
+        <p>💨 Viento: ${data.wind.speed} m/s</p>
+      `;
+    })
+    .catch(error => console.error("Error:", error));
+
+    document.getElementById("borrarclima").addEventListener("click", function() {
+  document.getElementById("resultado").innerHTML = " ";
+  document.getElementById("")
+
+});
+
+    
+});
